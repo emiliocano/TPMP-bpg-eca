7f997b8ec2319c39462bb9e46aed4c584ce82c2e
emiliocano <1125384+emiliocano@users.noreply.github.com>
2026-09-18 19:04:35 +0200

    primer commit

---
+++ Nucleo/Modelo/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nucleo.Modelo
+{
+    public class Usuario
+    {
+        private int id;
+        private string cuenta;
+        private string eMail;
+        
+        public Usuario(int id, string cuenta, string eMail)
+        {
+            this.id = id;
+            this.cuenta = cuenta;
+            this.eMail = eMail;
+        }
+
+        public int Id { get { return this.id; } set { this.id = value; } }
+    }
+}
